f0379a834be3b6f386d41ffe5abd00b2fa5d0954
Abdul Basit Ali <aliabdulbasit2002@gmail.com>
2024-11-21 16:46:41 +0000

    solara

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,49 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: "https://codetrainafrica.com/about-us",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://codetrainafrica.com/courses/software-engineering",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://codetrainafrica.com/courses/ui-ux-design",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://codetrainafrica.com/courses/codetrain-solara",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://codetrainafrica.com/success-stories",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://codetrainafrica.com/hire-a-graduate",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://codetrainafrica.com/partnerships",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://codetrainafrica.com/donate",
+      url: "https://codetrainsolara.com",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
